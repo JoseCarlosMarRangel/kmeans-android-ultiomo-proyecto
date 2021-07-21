82ad81f98042aafd1bb956b226ee7cbb6f6bc07b
José Carlos Mar Rangel <68357760+JoseCarlosMarRangel@users.noreply.github.com>
2021-07-21 18:51:02 -0500

    avances de proyecto
    se pudo incluir librerias opencp

--- app/src/main/java/com/example/kmeans/MainActivity.java
+++ app/src/main/java/com/example/kmeans/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.kmeans;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,25 +11,40 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnCamara;
+    Button cargafoto;
     ImageView imgView;
+
+    private static final int SELECT_FILE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnCamara = findViewById(R.id.btnCamara);
+        cargafoto = findViewById(R.id.cargafoto);
         imgView = findViewById(R.id.imageView);
 
         btnCamara.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 abrirCamara();
+            }
+        });
+
+        cargafoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
     }
@@ -40,12 +56,18 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+
+
         if (requestCode == 1 && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imgBitmap = (Bitmap) extras.get("data");
             imgView.setImageBitmap(imgBitmap);
         }
+
     }
 }
